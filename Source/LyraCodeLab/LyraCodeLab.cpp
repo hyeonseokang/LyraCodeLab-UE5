55081269b515d1d8b5f4601014267ed620d87e0f
hyunseo <hansx2079@naver.com>
2025-07-04 19:29:22 +0900

    LogChannel 추가

--- Source/LyraCodeLab/LyraCodeLab.cpp
+++ Source/LyraCodeLab/LyraCodeLab.cpp
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LyraCodeLab.h"
+#include "LyraCodeLabLogChannels.h"
 #include "Modules/ModuleManager.h"
 
 class FLyraCodeLabModule : public FDefaultGameModuleImpl
@@ -8,12 +9,12 @@ class FLyraCodeLabModule : public FDefaultGameModuleImpl
 public:
 	virtual void StartupModule() override
 	{
-		UE_LOG(LogTemp, Log, TEXT("Startup Module: LyraCodeLab"));
+		UE_LOG(LogLyraCodeLab, Log, TEXT("Startup Module: LyraCodeLab"));
 	}
 
 	virtual void ShutdownModule() override
 	{
-		UE_LOG(LogTemp, Log, TEXT("Shutdown Module: LyraCodeLab"));
+		UE_LOG(LogLyraCodeLab, Log, TEXT("Shutdown Module: LyraCodeLab"));
 	}
 };
 
