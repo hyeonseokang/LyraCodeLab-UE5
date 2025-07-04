885dd613ad699a75859c298d4052688262ecfba1
hyunseo <hansx2079@naver.com>
2025-07-04 19:01:18 +0900

    Module 세팅

--- Source/LyraCodeLab/LyraCodeLab.cpp
+++ Source/LyraCodeLab/LyraCodeLab.cpp
@@ -3,4 +3,19 @@
 #include "LyraCodeLab.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LyraCodeLab, "LyraCodeLab" );
+class FLyraCodeLabModule : public FDefaultGameModuleImpl
+{
+public:
+	virtual void StartupModule() override
+	{
+		UE_LOG(LogTemp, Log, TEXT("Startup Module: LyraCodeLab"));
+	}
+
+	virtual void ShutdownModule() override
+	{
+		UE_LOG(LogTemp, Log, TEXT("Shutdown Module: LyraCodeLab"));
+	}
+};
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FLyraCodeLabModule, LyraCodeLab, "LyraCodeLab");
