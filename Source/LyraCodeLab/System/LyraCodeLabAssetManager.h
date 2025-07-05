23108fa57742666c99fb125acc180acbe852387c
hyunseo <hansx2079@naver.com>
2025-07-05 17:46:24 +0900

    LyraCodeLabAssetManager 추가

---
+++ Source/LyraCodeLab/System/LyraCodeLabAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "LyraCodeLabAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LYRACODELAB_API ULyraCodeLabAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
