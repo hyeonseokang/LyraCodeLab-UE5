9546599041913b3dbc07a8344a486c5afc633400
hyunseo <hansx2079@naver.com>
2025-07-10 02:42:09 +0900

    AssetManager SynchronousLoad 추가

--- Source/LyraCodeLab/System/LyraCodeLabAssetManager.h
+++ Source/LyraCodeLab/System/LyraCodeLabAssetManager.h
@@ -13,5 +13,74 @@ UCLASS()
 class LYRACODELAB_API ULyraCodeLabAssetManager : public UAssetManager
 {
 	GENERATED_BODY()
-	
+
+public:
+	ULyraCodeLabAssetManager();
+
+	static ULyraCodeLabAssetManager& Get();
+
+	static bool ShouldLogAssetLoads();
+
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+
+	template <typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	template <typename AssetType>
+	static TSubclassOf<AssetType> GetSubclass(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	void AddLoadedAsset(const UObject* Asset);
+
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	FCriticalSection SyncObject;
 };
+
+template <typename AssetType>
+AssetType* ULyraCodeLabAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+	if (AssetPath.IsValid())
+	{
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset {%s}"), *AssetPointer.ToString());
+		}
+
+		if (LoadedAsset && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+
+	return LoadedAsset;
+}
+
+template <typename AssetType>
+TSubclassOf<AssetType> ULyraCodeLabAssetManager::GetSubclass(const TSoftObjectPtr<AssetType>& AssetPointer,
+                                                             bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset {%s}"), *AssetPointer.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+	
+	return LoadedSubclass;
+}
