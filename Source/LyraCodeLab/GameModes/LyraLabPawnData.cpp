0e0f8c65ed750f9485bf9c13b989c2189d9d7587
hyunseo <hansx2079@naver.com>
2025-07-26 04:38:51 +0900

    파일 오류 정리

--- Source/LyraCodeLab/GameModes/LyraLabPawnData.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GameModes/LyraLabPawnData.h"
-
-ULyraLabPawnData::ULyraLabPawnData(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
-{
-}
